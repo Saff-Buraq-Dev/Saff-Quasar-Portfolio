da3bddacb16bcb9d4e6cb272ba66f527b4c69121
Saff-Buraq-Dev <gharbi.safwen@hotmail.com>
2022-12-12 20:55:49 -0500

    mise à jour après stage

--- script.js
+++ script.js
@@ -1,38 +1,36 @@
-console.log("It's working")
-
 let theme = localStorage.getItem('theme')
 
-if(theme == null){
+if (theme == null) {
 	setTheme('light')
-}else{
+} else {
 	setTheme(theme)
 }
 
 let themeDots = document.getElementsByClassName('theme-dot')
 
 
-for (var i=0; themeDots.length > i; i++){
-	themeDots[i].addEventListener('click', function(){
+for (var i = 0; themeDots.length > i; i++) {
+	themeDots[i].addEventListener('click', function () {
 		let mode = this.dataset.mode;
 		console.log('Option clicked:', mode);
 		setTheme(mode);
 	})
 }
 
-function setTheme(mode){
-	if(mode == 'light'){
+function setTheme(mode) {
+	if (mode == 'light') {
 		document.getElementById('theme-style').href = 'default.css'
 	}
 
-	if(mode == 'blue'){
+	if (mode == 'blue') {
 		document.getElementById('theme-style').href = 'blue.css'
 	}
 
-	if(mode == 'green'){
+	if (mode == 'green') {
 		document.getElementById('theme-style').href = 'green.css'
 	}
 
-	if(mode == 'purple'){
+	if (mode == 'purple') {
 		document.getElementById('theme-style').href = 'purple.css'
 	}
 
